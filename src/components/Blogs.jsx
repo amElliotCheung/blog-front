5cb562612a91b44f026be941baef01c09329409c
elliot <to.elliot.cheung@gmail.com>
2022-11-15 23:09:21 +0900

    try to fetch

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -7,16 +7,17 @@ const Blogs = () => {
     const [blogs, setBlogs] = useState([])
     useEffect(() => {
         const getBlogs = async () => {
-            const res = await axios.get('https://url/blogs')
+            const res = await axios.get('http://13.231.114.243:8080/', {headers:{"Access-Control-Allow-Origin": "*"}})
+            console.log(res)
             setBlogs(res.data.items)
         }
         getBlogs()},  [])
 
     return (
         <div>
-            {blogs.length ===0 && <h3>haha</h3>}
+            {blogs.length ===0 && <h3>No</h3>}
             {blogs.map((blog) => 
-                <h3>blog.title</h3>
+                <h3>yes</h3>
             )}
         </div>
     )
